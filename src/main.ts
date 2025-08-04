5894fa1957dd5f09baac64d1812ab5de3bd1b931
Prashant Kumar <prashantkrmathur@gmail.com>
2025-08-04 22:19:42 +0530

    configured swagger documentation module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const allowedOrigins = [];
@@ -15,6 +16,18 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(bodyParser.json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Doctor Appointment Booking System')
+    .setDescription('API for managing doctors, time slots, and appointments')
+    .setVersion('1.0')
+    .addTag('Doctor', 'Endpoints for managing doctors')
+    .addTag('TimeSlot', 'Endpoints for managing time slots')
+    .addTag('Appointment', 'Endpoints for managing appointments')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
