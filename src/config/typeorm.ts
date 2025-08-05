dde8b5560b6a9e52e8732be633bcc87c2eb7d658
Prashant Kumar <prashantkrmathur@gmail.com>
2025-08-05 17:31:16 +0530

    changes in config for deployment

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,11 +6,12 @@ dotenvConfig({ path: '.env' });
 
 const config = {
     type: 'postgres',
-    host: `${process.env.DATABASE_HOST}`,
-    port: `${process.env.DATABASE_PORT}`,
-    username: `${process.env.DATABASE_USERNAME}`,
-    password: `${process.env.DATABASE_PASSWORD}`,
-    database: `${process.env.DATABASE_NAME}`,
+    url: `${process.env.DATABASE_URL}`,
+    // host: `${process.env.DATABASE_HOST}`,
+    // port: `${process.env.DATABASE_PORT}`,
+    // username: `${process.env.DATABASE_USERNAME}`,
+    // password: `${process.env.DATABASE_PASSWORD}`,
+    // database: `${process.env.DATABASE_NAME}`,
     entities: [__dirname + '/../entities/**/*{.ts,.js}'], 
     migrations: [__dirname + '/../migrations/*{.ts,.js}'], 
     autoLoadEntities: true,
